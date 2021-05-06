5c71e39607bca7e0408af9c69da82d73d74793bb
EmilyLow <emlow@me.com>
2021-05-06 16:00:23 -0500

    Basic axios connection

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import styled from "styled-components";
 
 import Schedule from "./components/Schedule";
 
+import axios from 'axios';
+import { useEffect } from "react";
+
 function App() {
+  const url = 'http://localhost:3001/events'
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+  
+
+  const getEvents = () => {
+    axios.get(url)
+    .then((response => {
+      console.log(response.data);
+    }))
+    .catch(error => console.error(`Error: ${error}`))
+  }
+
+
+
   return (
     <ScheduleDiv>
         <StyledH1>Convention Schedule</StyledH1>
